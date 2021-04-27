21e27d090cee2d56055bbe699eb9ade4a93fa949
SYUMJOBA <61433707+SYUMJOBA@users.noreply.github.com>
2021-04-27 09:42:45 +0200

    started worling more on the letterdict class Experimenting on the unicode special greek chars too (don't know why, but it did not end well)

--- letterDict.h
+++ letterDict.h
@@ -1,5 +1,7 @@
 class LetterDict {
     private:
+    string Keys[26] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+    string Values[26] = {"α"};
 
     public:
 };
---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main (){
+    string x;
+    cout << "αβ" << endl;
+    cout << "Press ENTER to exit" << endl;
+    getline(cin, x);
+}
