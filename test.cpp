4813e60c6035a1891c78463682ccb4e77bcfd76d
SYUMJOBA <61433707+SYUMJOBA@users.noreply.github.com>
2021-05-03 17:50:43 +0200

    I tried ...

--- test.cpp
+++ test.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <string>
+#include <fcntl.h>
+#include <io.h>
 using namespace std;
 
 int main (){
+    _set_mode(_fileno(stdout), _O_U16TEXT);
     string x;
-    cout << "\u03B1" << endl;
-    cout << "Press ENTER to exit" << endl;
+    wcout << "\x03b2" << endl;
     getline(cin, x);
 }
